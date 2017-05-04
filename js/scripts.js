53861f94d1661faebad456359e19baa8580f445e
Brendan Hanna and Beth Hansen <brendan@>
2017-05-04 15:11:01 -0700

    accomplish 1st spec: take one-digit numerals and convert them into roman numerals using a for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,46 @@
-//back end
-var I = 1
-var V = 5
-var X = 10
-var L = 50
-var C = 100
-var D = 500
-var M = 1000
+//
+
+// var input = $parseInt("#input").val();
 
 
+
+
+// foo[input - 1] = VIII
+//
+// take an input, loop through the numeral array, and divide (modulo)
+// by each number in that array
+//
+// newArray.push(foo[i]) // pushes "I" into a new array
+//
+//
+//
+
+// //back end
+var romanSingleDigit = ["0", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX",]
+var numberSingleDigit = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+function romanizeSingleDigit(x){
+  for(var index=0; index <= romanSingleDigit.length; index += 1){
+    if (x === index) {
+      return romanSingleDigit[index];
+    };
+  };
+};
 //front end
 $(function(){
 
   $("#user-form").submit(function(event){
     event.preventDefault();
-    var userInput = $("#user-input").val();
+    debugger;
 
-    var output = 
+    var userInput = parseInt($("#user-input").val());
 
-    $("#output").text(output);
+    var romanized = romanizeSingleDigit(userInput);
+
+    console.log(romanized);
+
+
+
   });
 
 });
