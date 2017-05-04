fbc9b139c8faf7282cc4e619816eb39d20b8ef50
Brendan Hanna and Beth Hansen <beth@>
2017-05-04 16:17:12 -0700

    attempt to turn user input from a parsed integer to a string so that we can split it into an array so that we can run a loop on each index that romanizes each digit

--- js/scripts.js
+++ js/scripts.js
@@ -19,25 +19,35 @@
 var romanSingleDigit = ["0", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX",]
 var numberSingleDigit = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-function romanizeSingleDigit(x){
-  for(var index=0; index <= romanSingleDigit.length; index += 1){
-    if (x === index) {
-      return romanSingleDigit[index];
+function romanizeSingleDigit(x){//this establishes a function called romanizeSingleDigit.
+  for(var index=0; index <= romanSingleDigit.length; index += 1){//tell the for loop to go through roman single digit array
+    if (x === index) {//for each loop if the current index is = to x (userInput)
+      return romanSingleDigit[index];//then return the value of the current index of array romanSingleDigit
     };
   };
 };
-//front end
-$(function(){
 
-  $("#user-form").submit(function(event){
-    event.preventDefault();
+function makeUserArray(number){
+  var ourNumber = number;
+  ourNumber.toString();
+  console.log(ourNumber);
+  // return string.split("");
+}
+
+//front end
+$(function(){//calling a fucntion
+
+  $("#user-form").submit(function(event){//the beginning of a fucntion when you submit a form
+    event.preventDefault();//stops from continuelly sending to a server
     debugger;
 
-    var userInput = parseInt($("#user-input").val());
+    var userInput = parseInt($("#user-input").val());//parses the user input and stores in a variable
 
-    var romanized = romanizeSingleDigit(userInput);
+    var userInputArray = makeUserArray(userInput);
 
-    console.log(romanized);
+    // var romanized = romanizeSingleDigit(userInput);//runs the user input through romanizeSingleDigit and returns it romanized
+
+    console.log(userInputArray);
 
 
 
